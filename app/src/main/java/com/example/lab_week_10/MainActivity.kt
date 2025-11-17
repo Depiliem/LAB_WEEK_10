19d29a749b499175450c900eecb603a523c294c5
Depiliem <davew1726@gmail.com>
2025-11-17 20:43:40 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -5,17 +5,42 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.lab_week_10.database.Total
+import com.example.lab_week_10.database.TotalDatabase
 import com.example.lab_week_10.viewmodels.TotalViewModel
 import com.example.lab_week_10.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val db by lazy { prepareDatabase() }
     private val viewModel by lazy {
         ViewModelProvider (this) [TotalViewModel::class.java]
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initializeValueFromDatabase()
         prepareViewModel()
+    }
+
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java, "total-database"
+        ).allowMainThreadQueries().build()
+    }
+
+    private fun initializeValueFromDatabase() {
+        val total = db.totalDao().getTotal(ID)
+        if (total.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal (total.first().total)
+        }
+    }
+
+    companion object {
+        const val ID: Long = 1
     }
 
     private fun updateText(total: Int) {
@@ -30,5 +55,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update (Total (ID, viewModel.total.value!!))
     }
 }
